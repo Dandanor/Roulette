b818493f569796c39c778178052db3a5edd5a5cc
Dandanor <Dandanor2003@gmail.com>
2025-10-05 01:23:19 +0300

    add betting system

--- Roulette.py
+++ Roulette.py
@@ -2,8 +2,13 @@ import random
 arr= [*range(1,37),'0','00']
 
 spin=random.choice(arr)
-x=int(input("enter your bet "))
-if x==spin:
+x=input("enter your bet ").capitalize()
+
+if x=="Red" and spin%2==1:
+    print (f"the winning number is {spin}, you win")
+elif x=="Black" and spin%2==0:
+    print (f"the winning number is {spin}, you win")
+elif x=="Green" and (spin=='0' or spin=='00'):
     print (f"the winning number is {spin}, you win")
 else:
     print (f"the winning number is {spin}, you lose")
